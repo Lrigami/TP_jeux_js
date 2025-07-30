8b4de70be9213ec42e5a020bc6dbd7bdf8a97889
Thiry Prune <prune.thiry@hotmail.com>
2025-07-30 11:14:47 +0200

    feat: user can add and delete character

---
+++ javascript/functions.js
@@ -0,0 +1,27 @@
+// js pour le ajoutPerso.html
+let getPersos = localStorage.getItem("allPersos");
+let personnages = getPersos ? JSON.parse(getPersos, 'UTF-8') : [];
+
+// create a new personnage 
+let newPersoForm = document.getElementById("new-perso-form");
+
+function createPerso() {
+    let newPerso = {};
+    newPerso.nom = document.getElementById("perso-name").value;
+    newPerso.classe = document.getElementById("perso-classe").value;
+    newPerso.arme = document.getElementById("perso-arme").value;
+    newPerso.constitution = document.getElementById("perso-constitution").value;
+    newPerso.force = document.getElementById("perso-force").value;
+    newPerso.agilite = document.getElementById("perso-agilite").value;
+    newPerso.intelligence = document.getElementById("perso-intelligence").value;
+    newPerso.sagesse = document.getElementById("perso-sagesse").value;
+    newPerso.charisme = document.getElementById("perso-charisme").value;
+    personnages.push(newPerso);
+    savePersos(personnages);
+}
+
+// save all personnages
+function savePersos(personnages) {
+    let allPersos = JSON.stringify(personnages);
+    localStorage.setItem("allPersos", allPersos);
+}
--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,12 @@
+// js pour l'index.html
 let persoList = document.getElementById("perso-list");
 let persoCardTemplate = document.querySelector("template");
-console.log(persoCardTemplate);
-
-let getPersos = localStorage.getItem("allPersos");
-console.log(getPersos);
-let personnages = getPersos ? JSON.parse(getPersos, 'UTF-8') : [];
 
 // create cards based on personnages array 
-function createPersoCards(perso) {
+personnages.forEach((perso, index) => createPersoCards(perso, index));  
+
+function createPersoCards(perso, index) {
     let persoCard = persoCardTemplate.content.cloneNode(true);
-    console.log(persoCard);
-    console.log(persoCard.h2);
     persoCard.querySelector("h2").textContent = perso.nom
 
     let persoP = persoCard.querySelectorAll("p span");
@@ -19,13 +15,14 @@ function createPersoCards(perso) {
         persoP[i-1].textContent = valuesNb[i];
     }
 
+    let supBtn = persoCard.querySelector(".btn-danger");
+    supBtn.setAttribute("onclick", `deletePerso(${index})`);
     persoList.appendChild(persoCard);
 }
 
-personnages.forEach(perso => createPersoCards(perso));  
-
-// save all personnages
-function savePersos(personnages) {
-    let allPersos = JSON.stringify(personnages);
-    localStorage.setItem("allPersos", allPersos);
+// supprimer un personnage
+function deletePerso(indice) {
+    personnages.splice(indice,1);
+    savePersos(personnages);
+    document.location.reload();
 }
